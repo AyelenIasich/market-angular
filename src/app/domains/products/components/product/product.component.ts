a4a28c6e5a2254250f06ea5a6549bd6117631c08
AyelenIasich <ayeleniasich@gmail.com>
2025-02-01 18:37:05 -0300

    Input decorations

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  @Input({required:true}) imgPhoto: string = '';
+  @Input({required:true}) title: string = '';
+  @Input({required:true}) price: number = 0;
+
+
+}
---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+
+export class ListComponent {
+  imgPhoto = 'https://picsum.photos/250/250?r=' + Math.random();
+}
